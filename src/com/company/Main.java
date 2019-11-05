95f874a2cfd9bfd8f6f2a25407753f6ca54e4e0b
Alexander William Verhulst <ftrr2017@gmail.com>
2019-11-05 17:57:42 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static int fibo (int n){
+        if (n ==1 || n==0) {
+            return 1;
+        }
+        else {
+            return fibo(n-1) + fibo(n - 2);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(fibo(13));
+    }
+}
